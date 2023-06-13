e881e3f2c3e2897f7c21d10c60cb1530d065353c
AuxXxilium <info@auxxxilium.tech>
2023-06-13 10:20:26 +0200

    vm: full update
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- docker.sh
+++ docker.sh
@@ -2,11 +2,11 @@
 
 make setup
 cp -r /spksrc/sponge /usr/bin/
-cd /spksrc/spk/open-vm-tools-12/ && make arch-apollolake-7.2
-cd /spksrc/spk/open-vm-tools-12/ && make arch-broadwell-7.2
-cd /spksrc/spk/open-vm-tools-12/ && make arch-broadwellnk-7.2
-cd /spksrc/spk/open-vm-tools-12/ && make arch-denverton-7.2
-cd /spksrc/spk/open-vm-tools-12/ && make arch-geminilake-7.2
-cd /spksrc/spk/open-vm-tools-12/ && make arch-r1000-7.2
-cd /spksrc/spk/open-vm-tools-12/ && make arch-v1000-7.2
+cd /spksrc/spk/open-vm-tools/ && make arch-apollolake-7.2
+cd /spksrc/spk/open-vm-tools/ && make arch-broadwell-7.2
+cd /spksrc/spk/open-vm-tools/ && make arch-broadwellnk-7.2
+cd /spksrc/spk/open-vm-tools/ && make arch-denverton-7.2
+cd /spksrc/spk/open-vm-tools/ && make arch-geminilake-7.2
+cd /spksrc/spk/open-vm-tools/ && make arch-r1000-7.2
+cd /spksrc/spk/open-vm-tools/ && make arch-v1000-7.2
 exit
