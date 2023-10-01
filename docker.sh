982aeab3f9e846153b5e3d94f9bb6299da2a113c
AuxXxilium <info@auxxxilium.tech>
2023-10-01 20:12:46 +0200

    tree: update
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- docker.sh
+++ docker.sh
@@ -5,8 +5,11 @@ cp -r /spksrc/sponge /usr/bin/
 cd /spksrc/spk/open-vm-tools/ && make arch-apollolake-7.1
 cd /spksrc/spk/open-vm-tools/ && make arch-broadwell-7.1
 cd /spksrc/spk/open-vm-tools/ && make arch-broadwellnk-7.1
+cd /spksrc/spk/open-vm-tools/ && make arch-broadwellnkv2-7.1
+cd /spksrc/spk/open-vm-tools/ && make arch-broadwellntbap-7.1
 cd /spksrc/spk/open-vm-tools/ && make arch-denverton-7.1
 cd /spksrc/spk/open-vm-tools/ && make arch-geminilake-7.1
+cd /spksrc/spk/open-vm-tools/ && make arch-purley-7.1
 cd /spksrc/spk/open-vm-tools/ && make arch-r1000-7.1
 cd /spksrc/spk/open-vm-tools/ && make arch-v1000-7.1
 exit
