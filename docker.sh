9e4c5f450286ea1307ab8b136688e71ce9e7723f
AuxXxilium <info@auxxxilium.tech>
2023-12-17 13:05:32 +0100

    build: add epyc7002
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- docker.sh
+++ docker.sh
@@ -12,4 +12,5 @@ cd /spksrc/spk/open-vm-tools/ && make arch-geminilake-7.1
 cd /spksrc/spk/open-vm-tools/ && make arch-purley-7.1
 cd /spksrc/spk/open-vm-tools/ && make arch-r1000-7.1
 cd /spksrc/spk/open-vm-tools/ && make arch-v1000-7.1
+cd /spksrc/spk/open-vm-tools/ && make arch-epyc7002-7.1
 exit
